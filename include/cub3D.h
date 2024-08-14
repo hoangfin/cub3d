ef149b0f090f810fad005d421e200e74a0a90a65
ejarvinen <emmi.jarvinen@proton.me>
2024-08-14 14:38:00 +0300

    Added MLX42 images as member of struct s_map

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 14:22:06 by emansoor          #+#    #+#             */
-/*   Updated: 2024/08/09 14:16:52 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/08/14 14:19:46 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,21 @@
 typedef struct s_map
 {
 	char		**grid;
-	int			width;
-	int			height;
-	int			row_count;
-	int			col_count;
-	mlx_image_t	*north_wall;
-	mlx_image_t	*south_wall;
-	mlx_image_t	*east_wall;
-	mlx_image_t	*west_wall;
-	int			floor_colors[3];
-	int			ceiling_colors[3];
+	uint32_t	width;
+	uint32_t	height;
+	uint32_t	row_count;
+	uint32_t	col_count;
+	uint32_t	color_floor;
+	uint32_t	color_ceiling;
+	mlx_image_t	*walls[4];
+	mlx_image_t	*ceiling;
+	mlx_image_t	*floor;
+	mlx_image_t	*minimap;
+	mlx_image_t	*map;
+	mlx_image_t	*map_wall;
+	mlx_image_t	*map_player;
+	mlx_image_t	*map_path;
+	mlx_image_t	*map_space;
 }	t_map;
 
 typedef struct s_ray
@@ -84,5 +89,10 @@ t_character	*new_character(mlx_t *mlx, int32_t x, int32_t y, int32_t angle);
 int		init(t_cub3D *cub3D, const char *pathname);
 int		start(t_cub3D *cub3D);
 void	destroy(t_cub3D *cub3D);
+
+uint32_t	color(int32_t r, int32_t g, int32_t b, int32_t a);
+void		fill(mlx_image_t *image, uint32_t color);
+mlx_image_t	*load_png(mlx_t *mlx, const char *pathname);
+void		copy_pixels(mlx_image_t *dest,mlx_image_t *src,int32_t offset);
 
 #endif

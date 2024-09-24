42813daa392174d0841359d975c1ad32c01fc8eb
ejarvinen <emmi.jarvinen@proton.me>
2024-09-24 17:24:09 +0300

    Draws odd-sized textures with corrected resolution

--- src/analyser/set_hit_texture.c
+++ src/analyser/set_hit_texture.c
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:42:17 by hoatran           #+#    #+#             */
-/*   Updated: 2024/09/24 12:25:23 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/09/24 17:22:08 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 static void	set_wall_texture(t_ray *ray, mlx_image_t *walls[])
 {
-	int32_t	pos_index;
+	double	pos_index;
 
 	if (ray->hit_side == 0)
 	{
 		ray->hit_texture = walls[3];
 		if (ray->x_end < ray->x_start)
 			ray->hit_texture = walls[1];
-		pos_index = (int32_t)ray->y_end % MAP_CELL_SIZE;
+		pos_index = fmod(ray->y_end, MAP_CELL_SIZE);
 		ray->hit_texture_pos_x = pos_index * ray->hit_texture->width / MAP_CELL_SIZE;
 	}
 	else
@@ -29,7 +29,7 @@ static void	set_wall_texture(t_ray *ray, mlx_image_t *walls[])
 		ray->hit_texture = walls[0];
 		if (ray->y_end < ray->y_start)
 			ray->hit_texture = walls[2];
-		pos_index = (int32_t)ray->x_end % MAP_CELL_SIZE;
+		pos_index = fmod(ray->x_end, MAP_CELL_SIZE);
 		ray->hit_texture_pos_x = pos_index * ray->hit_texture->width / MAP_CELL_SIZE;
 	}
 }

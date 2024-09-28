97e8db2de5d8b758d2b5ccafd9c93e43ce72aaba
hoangfin <hoang.tran.fin@gmail.com>
2024-09-28 22:25:48 +0300

    Added free(doors) and mlx_delete_image(...asset.door)

--- src/cub/destroy.c
+++ src/cub/destroy.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 20:08:54 by hoatran           #+#    #+#             */
-/*   Updated: 2024/09/27 16:56:22 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/09/28 22:20:12 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static void	delete_assets(t_cub3D *cub3d)
 	while (i < 4)
 		mlx_delete_image(cub3d->mlx, cub3d->asset.walls[i++]);
 	mlx_delete_image(cub3d->mlx, cub3d->asset.obstacle);
+	mlx_delete_image(cub3d->mlx, cub3d->asset.door);
 	mlx_delete_image(cub3d->mlx, cub3d->asset.navigator);
 	delete_sprite(cub3d->asset.sprite_door, cub3d->mlx);
 	delete_sprite(cub3d->asset.sprite_wand, cub3d->mlx);
@@ -41,6 +42,7 @@ void	destroy(t_cub3D *cub3d)
 	if (cub3d->mlx != NULL)
 		mlx_close_window(cub3d->mlx);
 	delete_map(cub3d->map);
+	free(cub3d->doors);
 	delete_images(cub3d);
 	delete_assets(cub3d);
 	free(cub3d->rays);
